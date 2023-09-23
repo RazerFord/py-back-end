9d889bbd5969cb2d53bf0d82d1be5dc14c6f855e
RazerFord <desk10567@gmail.com>
2023-09-23 15:35:00 +0300

    feat: added api

--- app/api.py
+++ app/api.py
@@ -1,8 +1,53 @@
 from fastapi import FastAPI
+from message import Message
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/greeting")
+async def greeting_world():
+    """
+    Greet the world
+    """
+    return {"greeting": "Hello, world!"}
+
+
+@app.get("/greeting/{name}")
+async def greeting(name: str):
+    """
+    Greet the user:
+
+    **Path params**:
+    - **name**: username
+    """
+    return {"greeting": f"Hello, {name}!"}
+
+
+@app.get("/biography")
+async def biography(name: str, age: int = None, location: str = None):
+    """
+    Create a biography from data:
+
+    **Query params**:
+    - **name**: username
+    - **age**: user age
+    - **location**: user location
+    """
+    info = f"I'm {name}."
+    if age:
+        info += f" I'm {age} years old."
+    if location:
+        info += f" I'm from {location}."
+    return {"biography": info}
+
+
+@app.post("/message/{group_id}")
+async def message(group_id: int, message: Message):
+    """
+    Send a message to the group:
+
+    **Body parameters**:
+    - **name**: message sender name
+    - **text**: message text
+    """
+    return {"group_id": group_id, "message": message}
